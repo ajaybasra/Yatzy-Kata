38cd008bb3e773ca90e4500174baccdd86156791
Ajay Basra <ajay_b@hotmail.co.nz>
2023-03-21 09:48:22 +1300

    completed tests and implementation for category-score-calculator class

--- Yatzy/CategoryScoreCalculator.cs
+++ Yatzy/CategoryScoreCalculator.cs
@@ -2,19 +2,90 @@ namespace Yatzy;
 
 public class CategoryScoreCalculator
 {
-    public int GetChanceScore(int[] DiceRoll)
+    public int GetChanceScore(int[] diceRoll)
     {
-        return DiceRoll.Sum();
+        return diceRoll.Sum();
     }
     
-    public int GetYatzyScore(int[] DiceRoll)
+    public int GetYatzyScore(int[] diceRoll)
     {
-        var yatzyValue = DiceRoll.First();
-        return DiceRoll.All(dieValue=> dieValue == yatzyValue) ? 50 : 0;
+        var yatzyValue = diceRoll.First();
+        return diceRoll.All(dieValue=> dieValue == yatzyValue) ? 50 : 0;
     }
 
-    public int GetXScore(int[] DiceRoll, int number)
+    public int GetXScore(int[] diceRoll, int number)
     {
-        return DiceRoll.Sum(dieValue => dieValue == number ? dieValue : 0);
+        return diceRoll.Sum(dieValue => dieValue == number ? dieValue : 0);
+    }
+
+    public int GetPairScore(int[] diceRoll)
+    {
+        var duplicateDiceValues = GetDuplicateDiceValues(diceRoll);
+        return duplicateDiceValues.Length > 0 ? duplicateDiceValues.Max() * 2 : 0;
+    }
+
+    public int GetTwoPairScore(int[] diceRoll)
+    {
+        var duplicateDiceValues = GetDuplicateDiceValues(diceRoll);
+        return duplicateDiceValues.Length > 1 ? duplicateDiceValues[^1] * 2 + duplicateDiceValues[^2] * 2 : 0;
+    }
+
+    private int[] GetDuplicateDiceValues(IEnumerable<int> diceRoll)
+    {
+        IEnumerable<int> checkDuplicates = diceRoll.GroupBy(x => x)
+            .Where(g => g.Count() > 1)
+            .Select(x => x.Key);
+        
+        return (checkDuplicates.ToArray());
+    }
+
+    public int GetXOfAKindScore(int[] diceRoll, int xValue)
+    {
+        if (xValue != 3 && xValue != 4)
+        {
+            return 0;
+        }
+        
+        var numberCount = diceRoll.GroupBy(g => g).Where(x => x.Count() >= xValue).Select(g => g.Key).ToList();
+        return numberCount.Any() ? numberCount[0] * xValue : 0;
+    }
+    
+    public int GetSmallStraightScore(int[] diceRoll)
+    {
+        var startFromZero = diceRoll[..4];
+        var startFromOne = diceRoll[1..];
+
+        if (startFromZero.SequenceEqual(new[] { 1, 2, 3, 4 }) || startFromZero.SequenceEqual(new[] { 2, 3, 4, 5 }) ||
+            startFromZero.SequenceEqual(new[] { 3, 4, 5, 6 }))
+        {
+            return 30;
+        }
+        
+        if (startFromOne.SequenceEqual(new[] { 1, 2, 3, 4 }) || startFromOne.SequenceEqual(new[] { 2, 3, 4, 5 }) ||
+            startFromOne.SequenceEqual(new[] { 3, 4, 5, 6 }))
+        {
+            return 30;
+        }
+
+        return 0;
+    }
+    
+    public int GetLargeStraightScore(int[] diceRoll)
+    {
+        if (diceRoll.SequenceEqual(new[] {1, 2, 3, 4, 5}) || diceRoll.SequenceEqual(new[] {2, 3, 4, 5 ,6}))
+        {
+            return 40;
+        } else
+        {
+            return 0;
+        }
+    }
+    
+    public int GetFullHouseScore(int[] diceRoll)
+    {
+        var fullHousePairs = diceRoll.GroupBy(g => g).Where(x => x.Count() == 2).Select(g => g.Key);
+        var fullHouseThrees = diceRoll.GroupBy(g => g).Where(x => x.Count() == 3).Select(g => g.Key);
+
+        return fullHousePairs.Any() && fullHouseThrees.Any() ? diceRoll.Sum() : 0;
     }
 }
--- YatzyTests/CategoryScoreCalculatorTests.cs
+++ YatzyTests/CategoryScoreCalculatorTests.cs
@@ -40,4 +40,82 @@ public class CategoryScoreCalculatorTests
         
         Assert.Equal(expectedScore, actualScore);
     }
+    
+    [Theory]
+    [InlineData( new int[] { 1, 2, 3, 4, 5 }, 0 )]
+    [InlineData( new int[] { 2, 2, 2 ,4, 2 }, 4 )]
+    [InlineData( new int[] { 2, 2, 4 ,4, 2 }, 8 )]
+    public void GetPairScore_GivenDiceValues_ReturnCorrectOutput(int[] diceRolls , int expectedScore)
+    {
+        var actualScore = _categoryScoreCalculator.GetPairScore(diceRolls);
+        
+        Assert.Equal(expectedScore, actualScore);
+    }
+    
+    [Theory]
+    [InlineData( new int[] { 1, 2, 3, 4, 5 }, 0 )]
+    [InlineData( new int[] { 2, 2, 4 ,4, 6 }, 12 )]
+    [InlineData( new int[] { 3, 3, 3, 1, 1 }, 8 )]
+    public void GetTwoPairScore_GivenDiceValues_ReturnCorrectOutput(int[] diceRolls , int expectedScore)
+    {
+        var actualScore = _categoryScoreCalculator.GetTwoPairScore(diceRolls);
+        
+        Assert.Equal(expectedScore, actualScore);
+    }
+    
+    [Theory]
+    [InlineData( new int[] { 3, 3, 3, 4, 5 }, 3, 9 )]
+    [InlineData( new int[] { 3, 3, 4, 5, 6 }, 3, 0 )]
+    [InlineData( new int[] { 3, 3, 3, 3, 1 }, 3, 9 )]
+    [InlineData( new int[] { 2, 2, 2, 2, 5 }, 4, 8 )]
+    [InlineData( new int[] { 2, 2, 2, 5, 5 }, 4, 0 )]
+    [InlineData( new int[] { 2, 2, 2, 2, 2 }, 4, 8 )]
+    public void GetXOfAKindScore_GivenDiceValues_ReturnCorrectOutput(int[] diceRolls , int xValue, int expectedScore)
+    {
+        var actualScore = _categoryScoreCalculator.GetXOfAKindScore(diceRolls, xValue);
+        
+        Assert.Equal(expectedScore, actualScore);
+    }
+    
+    [Theory]
+    [InlineData( new int[] { 1, 2, 3, 4, 5 }, 30 )]
+    [InlineData( new int[] { 2, 3, 4 ,5, 0 }, 30 )]
+    [InlineData( new int[] { 1, 3, 4, 5, 6 }, 30 )]
+    [InlineData( new int[] { 1, 2, 3, 5, 4 }, 0 )]
+    [InlineData( new int[] { 4, 5, 6, 3, 6 }, 0 )]
+    public void GetSmallStraightScore_GivenDiceValues_ReturnCorrectOutput(int[] diceRolls , int expectedScore)
+    {
+        var actualScore = _categoryScoreCalculator.GetSmallStraightScore(diceRolls);
+        
+        Assert.Equal(expectedScore, actualScore);
+    }
+    
+    [Theory]
+    [InlineData( new int[] { 1, 2, 3, 4, 5 }, 40 )]
+    [InlineData( new int[] { 2, 3, 4, 5, 6 }, 40 )]
+    [InlineData( new int[] { 1, 2, 3, 4, 0 }, 0 )]
+    [InlineData( new int[] { 5, 1, 2, 3, 4 }, 0 )]
+    public void GetLargeStraightScore_GivenDiceValues_ReturnCorrectOutput(int[] diceRolls , int expectedScore)
+    {
+        var actualScore = _categoryScoreCalculator.GetLargeStraightScore(diceRolls);
+        
+        Assert.Equal(expectedScore, actualScore);
+
+    }
+    
+    [Theory]
+    [InlineData( new int[] { 1, 1, 2, 2, 2 }, 8 )]
+    [InlineData( new int[] { 2, 2, 3, 3, 4 }, 0 )]
+    [InlineData( new int[] { 4, 4, 4, 4, 4 }, 0 )]
+    [InlineData( new int[] { 1, 1, 2, 1, 2 }, 7 )]
+    public void GetFullHouseScore_GivenDiceValues_ReturnCorrectOutput(int[] diceRolls , int expectedScore)
+    {
+        var actualScore = _categoryScoreCalculator.GetFullHouseScore(diceRolls);
+        
+        Assert.Equal(expectedScore, actualScore);
+
+    }
+    
+    
 }
+ 
