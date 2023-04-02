eeb06f0b2e9a7ac32fc876f40f7cacabb5cbfe8e
Ajay Basra <ajay_b@hotmail.co.nz>
2023-04-03 11:08:28 +1200

    feature: added dictionary which can potentially replace nasty switch statement

--- Yatzy/PlayerCategories.cs
+++ Yatzy/PlayerCategories.cs
@@ -5,13 +5,37 @@ namespace Yatzy;
 public class PlayerCategories
 {
     public readonly List<Category> ListOfCategories = Enum.GetValues(typeof(Category)).Cast<Category>().ToList();
-    private readonly CategoryScoreCalculator _categoryScoreCalculator;
+    public readonly CategoryScoreCalculator _categoryScoreCalculator;
 
     public PlayerCategories(CategoryScoreCalculator categoryScoreCalculator)
     {
         _categoryScoreCalculator = categoryScoreCalculator;
     }
-    
+
+    public int GetScoreForPlacingRollInCategory(Category category, int[] diceRolls)
+    {
+        var CategoriesToScoreCalculatorDictionary = new Dictionary<Category, int>()
+        {
+            {Category.Chance, _categoryScoreCalculator.GetChanceScore(diceRolls)},
+            {Category.Yatzy, _categoryScoreCalculator.GetYatzyScore(diceRolls)},
+            {Category.Ones, _categoryScoreCalculator.GetXScore(diceRolls, 1)},
+            {Category.Twos, _categoryScoreCalculator.GetXScore(diceRolls, 2)},
+            {Category.Threes, _categoryScoreCalculator.GetXScore(diceRolls, 3)},
+            {Category.Fours, _categoryScoreCalculator.GetXScore(diceRolls, 4)},
+            {Category.Fives, _categoryScoreCalculator.GetXScore(diceRolls, 5)},
+            {Category.Sixes, _categoryScoreCalculator.GetXScore(diceRolls, 6)},
+            {Category.Pair, _categoryScoreCalculator.GetPairScore(diceRolls)},
+            {Category.TwoPairs, _categoryScoreCalculator.GetTwoPairScore(diceRolls)},
+            {Category.ThreeOfAKind, _categoryScoreCalculator.GetXOfAKindScore(diceRolls, 3)},
+            {Category.FourOfAKind, _categoryScoreCalculator.GetXOfAKindScore(diceRolls, 4)},
+            {Category.SmallStraight, _categoryScoreCalculator.GetSmallStraightScore(diceRolls)},
+            {Category.LargeStraight, _categoryScoreCalculator.GetLargeStraightScore(diceRolls)},
+            {Category.FullHouse, _categoryScoreCalculator.GetFullHouseScore(diceRolls)}
+        };
+
+        return CategoriesToScoreCalculatorDictionary[category];
+    }
+
     public int PlaceRollsInCategory(Category category, int[] diceRolls) 
     {
         switch (category)
