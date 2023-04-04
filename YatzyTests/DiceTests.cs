c44fb1043ec74114f54d600751ba73bf3146d118
Ajay Basra <ajay_b@hotmail.co.nz>
2023-04-04 15:29:21 +1200

    test: added more dice tests

--- YatzyTests/DiceTests.cs
+++ YatzyTests/DiceTests.cs
@@ -8,20 +8,20 @@ public class DiceTests
 {
 
     private readonly Mock<IRandomNumberGenerator> _mockRNG;
-    private readonly DiceRoll _diceRoller;
+    private readonly DiceRoll _diceRoll;
     private readonly Die _die;
     private readonly Die _dieNonMock;
 
     public DiceTests()
     {
         _mockRNG = new Mock<IRandomNumberGenerator>();
-        _diceRoller = new DiceRoll(new RNG());
+        _diceRoll = new DiceRoll(new RNG());
         _die = new Die(_mockRNG.Object);
         _dieNonMock = new Die(new RNG());
     }
     
     [Fact]
-    public void Die_RollDice_ShouldReturnWhatRNGSpitsOut() 
+    public void Die_RollDie_ShouldReturnWhatRNGSpitsOut() 
     {
         
         _mockRNG.Setup(x => x.GetRandomNumber()).Returns(1);
@@ -43,7 +43,7 @@ public class DiceTests
     public void DiceRoll_GetDiceRolls_ShouldReturnFiveDiceValues()
     {
 
-        var arrayOfDiceRolls = _diceRoller.GetDiceRolls();
+        var arrayOfDiceRolls = _diceRoll.GetDiceRolls();
         
         Assert.Equal(5, arrayOfDiceRolls.Length);
 
@@ -53,9 +53,9 @@ public class DiceTests
     public void DiceRoll_RollDice_ShouldReturnNewValues()
     {
 
-        var oldArrayOfDiceRolls = _diceRoller.GetDiceRolls();
-        _diceRoller.RollDice();
-        var newArrayOfDiceRolls = _diceRoller.GetDiceRolls();
+        var oldArrayOfDiceRolls = _diceRoll.GetDiceRolls();
+        _diceRoll.RollDice();
+        var newArrayOfDiceRolls = _diceRoll.GetDiceRolls();
         
         Assert.NotEqual(oldArrayOfDiceRolls, newArrayOfDiceRolls);
 
@@ -64,26 +64,48 @@ public class DiceTests
     [Fact]
     public void DiceRoll_RollDice_ShouldNotRollDieWhichAreHeld()
     {
-        var arrayOfDiceRolls = _diceRoller.GetDiceRolls();
+        var arrayOfDiceRolls = _diceRoll.GetDiceRolls();
         var originalFirstValue = arrayOfDiceRolls[0];
         var originalLastValue = arrayOfDiceRolls[4];
         
-        _diceRoller.HoldDice(new[] {0, 4});
-        _diceRoller.RollDice();
-        var actualFirstValue = _diceRoller.Dice[0].DieValue;
-        var actualLastValue = _diceRoller.Dice[4].DieValue;
+        _diceRoll.HoldDice(new[] {0, 4});
+        _diceRoll.RollDice();
+        var actualFirstValue = _diceRoll.Dice[0].DieValue;
+        var actualLastValue = _diceRoll.Dice[4].DieValue;
         
         Assert.Equal(originalFirstValue, actualFirstValue);
         Assert.Equal(originalLastValue, actualLastValue);
     }
+
+    [Fact]
+    public void DiceRoll_SetNumberOfRolls_ShouldUpdateValue()
+    {
+        _diceRoll.SetNumberOfRolls(1);
+        Assert.Equal(1, _diceRoll.NumberOfRollsLeft);
+        
+        _diceRoll.SetNumberOfRolls(3);
+        Assert.Equal(3, _diceRoll.NumberOfRollsLeft);
+    }
     
     [Fact]
-    public void DecrementRollsLeft_ShouldSubtractFromPlayerRolls_WhenCalled()
+    public void DiceRoll_DecrementRollsLeft_ShouldSubtractFromPlayerRolls()
     {
-        _diceRoller.SetNumberOfRolls(3);
-        _diceRoller.DecrementRollsLeft();
+        _diceRoll.SetNumberOfRolls(3);
+        _diceRoll.DecrementRollsLeft();
+        Assert.Equal(2, _diceRoll.NumberOfRollsLeft);
         
-        Assert.Equal(2, _diceRoller.NumberOfRollsLeft);
+        _diceRoll.SetNumberOfRolls(1);
+        _diceRoll.DecrementRollsLeft();
+        Assert.Equal(0, _diceRoll.NumberOfRollsLeft);
     }
- 
+
+    [Fact]
+    public void DiceRoll_IsRollsLeft_ShouldReturnCorrectBool()
+    {
+        _diceRoll.SetNumberOfRolls(1);
+        Assert.True(_diceRoll.IsRollsLeft());
+        
+        _diceRoll.SetNumberOfRolls(0);
+        Assert.False(_diceRoll.IsRollsLeft());
+    }
 }
